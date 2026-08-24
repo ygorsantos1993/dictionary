2c43126e3910f14662c7bea8f68b7b85ec62fa5d
ygorsantos1993 <connectapp2025@gmail.com>
2026-08-24 10:18:35 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL =
+  "https://bqgweziunlzwdousflkf.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_aZKwM2_YzG3OzL6KLnSg0A_XWrRYF1J";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
