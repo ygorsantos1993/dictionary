f94e6b7bbba8a0640715c944de4a506ebce6843c
ygorsantos1993 <connectapp2025@gmail.com>
2026-08-24 10:37:06 -0300

    Update dictionary.js

--- pages/js/dictionary.js
+++ pages/js/dictionary.js
@@ -8,40 +8,39 @@ const logoutButton =
 
 
 async function requireSession() {
-
   const {
     data,
     error
   } = await supabase.auth.getSession();
 
-
   if (
     error ||
     !data.session
   ) {
-
     window.location.href =
-      "../../index.html";
+      "../index.html";
 
     return;
   }
 
-
-  userEmail.textContent =
-    data.session.user.email;
+  if (userEmail) {
+    userEmail.textContent =
+      data.session.user.email;
+  }
 }
 
 
-logoutButton.addEventListener(
-  "click",
-  async () => {
+if (logoutButton) {
+  logoutButton.addEventListener(
+    "click",
+    async () => {
+      await supabase.auth.signOut();
 
-    await supabase.auth.signOut();
-
-    window.location.href =
-      "../../index.html";
-  }
-);
+      window.location.href =
+        "../index.html";
+    }
+  );
+}
 
 
 requireSession();
