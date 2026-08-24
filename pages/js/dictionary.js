96eaceadb89d8a5692fd62f560898aec4a05b898
ygorsantos1993 <connectapp2025@gmail.com>
2026-08-24 10:29:25 -0300

    Create dictionary.js

---
+++ pages/js/dictionary.js
@@ -0,0 +1,47 @@
+import { supabase } from "../../js/supabase.js";
+
+const userEmail =
+  document.getElementById("userEmail");
+
+const logoutButton =
+  document.getElementById("logoutButton");
+
+
+async function requireSession() {
+
+  const {
+    data,
+    error
+  } = await supabase.auth.getSession();
+
+
+  if (
+    error ||
+    !data.session
+  ) {
+
+    window.location.href =
+      "../../index.html";
+
+    return;
+  }
+
+
+  userEmail.textContent =
+    data.session.user.email;
+}
+
+
+logoutButton.addEventListener(
+  "click",
+  async () => {
+
+    await supabase.auth.signOut();
+
+    window.location.href =
+      "../../index.html";
+  }
+);
+
+
+requireSession();
